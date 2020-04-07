70c05e0e1a1701fe96dea6e5f5be429c38e35acf
rdcmji <rdcmji@gmail.com>
2020-04-07 08:53:41 +0000

    add saving user name part

--- form.js
+++ form.js
@@ -1,6 +1,42 @@
-const question = documnet.querySelector("span");
-function init(){
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greeting");
+const question = form.querySelector("span");
 
+const USER_LS = "currentUser";
+const SHOWING_CN = "showing";
+
+function saveName(text){
+  localStorage.setItem(USER_LS, text);
+}
+
+function askForName(){
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", function(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+  });
+}
+
+function paintGreeting(text){
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName(){
+  const currentUser = localStorage.getItem(USER_LS)
+  if(currentUser === null){
+    askForName();
+  }else{
+    paintGreeting(currentUser);
+  }
+}
+
+function init(){
+  loadName()
 }
 
 init();
